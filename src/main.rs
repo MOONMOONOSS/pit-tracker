9863584a323f663cf7a2520ba8c936c1eb18a321
Joseph Zurowski <12567900+Dunklheit@users.noreply.github.com>
2020-09-20 18:12:53 -0400

    Struct layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use serenity::model::{
+  channel::GuildChannel,
+  guild::Role,
+  id::UserId,
+  user::User,
+};
+use std::time::SystemTime;
+
+pub struct BotConfig {
+  pub settle_time: u16,
+  pub(crate) token: String,
+  pub warn_channel: GuildChannel,
+  pub warn_role: Role,
+  pub warn_threshold: u16,
+}
+
+pub struct BotState {
+  pub(self) bans: u64,
+  pub(self) pits: u64,
+  pub(self) unique_users: u64,
+  pub(self) uptime: SystemTime,
+  pub(self) users: Vec<PunishedUser>,
+}
+
+pub struct PunishedUser {
+  pub(self) id: UserId,
+  pub(self) user: User,
+  pub times_punished: u16,
+  pub last_punish: SystemTime,
+}
+
 fn main() {
   println!("Hello, world!");
 }
